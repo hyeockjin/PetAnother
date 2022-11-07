64b46be24c03a889b899b894e54be920a7c41ed3
Aeri <choyd00@naver.com>
2022-11-08 00:45:18 +0900

    s

--- app/src/main/java/com/lx/project5/CompleteFragment.kt
+++ app/src/main/java/com/lx/project5/CompleteFragment.kt
@@ -13,17 +13,20 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PayFragment.newInstance] factory method to
+ * Use the [CompleteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PayFragment : Fragment() {
+class CompleteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     override fun onCreateView(
@@ -31,7 +34,7 @@ class PayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pay, container, false)
+        return inflater.inflate(R.layout.fragment_complete, container, false)
     }
 
     companion object {
@@ -41,12 +44,12 @@ class PayFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PayFragment.
+         * @return A new instance of fragment CompleteFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PayFragment().apply {
+            CompleteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
