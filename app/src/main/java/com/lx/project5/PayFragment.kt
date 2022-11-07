ff620afe843d1f7748eecd6ceead02dc737abc64
Aeri <choyd00@naver.com>
2022-11-07 20:59:47 +0900

    ss

--- app/src/main/java/com/lx/project5/PayFragment.kt
+++ app/src/main/java/com/lx/project5/PayFragment.kt
@@ -23,10 +23,7 @@ class PayFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        var a =1
     }
 
     override fun onCreateView(
